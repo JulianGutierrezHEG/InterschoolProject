2c7930a9a6002e79d1a6529f8bbf0c0a1ffec0ac
JulianGutierrezHEG <71515734+JulianGutierrezHEG@users.noreply.github.com>
2022-06-04 17:49:57 +0200

    Delete eventAddict directory

--- eventAddict/routers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from base.viewsets import PlaceViewSet, EventViewSet
-
-router = routers.DefaultRouter()
-router.register(r'place', PlaceViewSet)
-router.register(r'event', EventViewSet)
